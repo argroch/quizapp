86eeb5692d58afa95a71abfc9013ab7bd7239f11
Aaron Groch <ar.groch@gmail.com>
2014-05-30 17:10:31 -0400

    first commit, TTS city quiz complete'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quizapp::Application.config.secret_key_base = 'f409bdb55503071f2a438b4139c90cfbcbca7bfd3acb7d3a6bad2f28e6da297b4d59d1a1aba2de8e15490509f956dbf476ab90dd894354c00490d73a6d30912d'
